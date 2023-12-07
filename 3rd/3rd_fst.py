b54652ecac5004feb6b28660a9b74bd812846080
Schmant13 <147262051+Schmant13@users.noreply.github.com>
2023-12-07 20:35:16 +0100

    3rd_fst.py aktualisieren
    Func appendAdjacentNums; main init

--- 3rd/3rd_fst.py
+++ 3rd/3rd_fst.py
@@ -5,10 +5,24 @@ file_to_open = source_folder / "input_3rd_test.txt"
 
 grid = open(file_to_open).read().splitlines()
 coordinates = set()
+numbers = []
 
-class Grid:
-    for x, row in enumerate(grid):
-        for y, column in enumerate (row):
-            pos_x = x
-            pos_y = y
-        pass
+def appendAdjacentNums(x,y,char,grid):
+    tempNum = ""
+    for x in [x-1,x,x+1]:
+        for y in [y-1,y,y+1]:
+            while grid[x-1][y].isdigit:
+                tempNum += char
+            number = int(tempNum)
+            numbers.append(number)
+                
+def main():
+	for x, row in enumerate (grid):
+    	for y, char in enumerate (grid):
+            if char.isdigit() or char == ".":
+                continue
+            else:
+                getAdjacentNums(x,y,char,grid)
+    print(sum(numbers))
+
+main()
