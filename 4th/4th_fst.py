6cbf4c560a052a5e1597cf871f8a17fd88a13437
Schmant13 <147262051+Schmant13@users.noreply.github.com>
2023-12-07 20:57:01 +0100

    4th_fst.py aktualisieren
    1st solution

--- 4th/4th_fst.py
+++ 4th/4th_fst.py
@@ -4,6 +4,24 @@ src_folder = Path("src")
 file_to_open = src_folder / "input_4th_test.txt"
 f = open(file_to_open).readline()
 
+total = 0
+
 for lines in f:
+    card = 0
+    #split "Card...:" from numbers
     line = lines.split(':')
-    print(line)
+    #split winning numbers from own numbers
+    nums = line[1].strip.split(" | ")
+    winningNums = nums[0].strip.split()
+    ownNums = nums[1].strip.split()
+    #check if own numbers in winning numbers + add to temporal variable "card")
+    for num in ownNums:
+        if num in winningNums:
+            if card == 0:
+                card += 1
+            else:
+                card *= 2
+    card += total
+    
+print(total)
+                
