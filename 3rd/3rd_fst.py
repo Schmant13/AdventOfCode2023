43edda40e094ee4b8afd57a00fe598ec162fbe97
Anton Mühlbach <antonmuehlbach@icloud.com>
2023-12-07 22:08:30 +0100

    4th1st: finished; 3rd compiling too long

--- 3rd/3rd_fst.py
+++ 3rd/3rd_fst.py
@@ -3,26 +3,27 @@ from pathlib import Path
 source_folder = Path("src")
 file_to_open = source_folder / "input_3rd_test.txt"
 
-grid = open(file_to_open).read().splitlines()
+grid = open(file_to_open).read.splitlines()
 coordinates = set()
 numbers = []
 
-def appendAdjacentNums(x,y,char,grid):
+def appendAdjacentNums(x,y,char,coordinateGrid):
     tempNum = ""
     for x in [x-1,x,x+1]:
         for y in [y-1,y,y+1]:
-            while grid[x-1][y].isdigit:
+            while coordinateGrid[x-1][y].isdigit:
                 tempNum += char
             number = int(tempNum)
             numbers.append(number)
                 
 def main():
-	for x, row in enumerate (grid):
-    	for y, char in enumerate (grid):
+    for x, row in enumerate (grid):
+        for y, char in enumerate (grid):
             if char.isdigit() or char == ".":
                 continue
             else:
-                getAdjacentNums(x,y,char,grid)
+                print(char)
+                appendAdjacentNums(x,y,char,grid)
     print(sum(numbers))
 
 main()
--- 4th/4th_fst.py
+++ 4th/4th_fst.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
 src_folder = Path("src")
-file_to_open = src_folder / "input_4th_test.txt"
-f = open(file_to_open).readline()
+file_to_open = src_folder / "input_4th.txt"
+f = open(file_to_open)
 
 total = 0
 
@@ -11,17 +11,21 @@ for lines in f:
     #split "Card...:" from numbers
     line = lines.split(':')
     #split winning numbers from own numbers
-    nums = line[1].strip.split(" | ")
-    winningNums = nums[0].strip.split()
-    ownNums = nums[1].strip.split()
+    nums = line[1].strip().split(" | ")
+    winningNums = nums[0].strip().split()
+    ownNums = nums[1].strip().split()
     #check if own numbers in winning numbers + add to temporal variable "card")
     for num in ownNums:
         if num in winningNums:
             if card == 0:
                 card += 1
+                print(num, winningNums)
+                print(card)
             else:
                 card *= 2
-    card += total
+                print(num, winningNums)
+                print(card)
+    total += card
     
 print(total)
                 
