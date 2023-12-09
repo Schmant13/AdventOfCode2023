3fb507785791a44e63ed7b1717d832175956b953
Anton Mühlbach <antonmuehlbach@icloud.com>
2023-12-09 19:51:24 +0100

    4th both parts merged

--- 4th/4th_fst.py
+++ 4th/4th_fst.py
@@ -1,31 +1,54 @@
 from pathlib import Path
 
 src_folder = Path("src")
-file_to_open = src_folder / "input_4th.txt"
+file_to_open = src_folder / "input_4th_test.txt"
 f = open(file_to_open)
 
-total = 0
+def part1():
+    total = 0
+    for lines in f:
+        card = 0
+        #split "Card...:" from numbers
+        line = lines.split(':')
+        #split winning numbers from own numbers
+        nums = line[1].strip().split(" | ")
+        winningNums = nums[0].strip().split()
+        ownNums = nums[1].strip().split()
+        #check if own numbers in winning numbers + add to temporal variable "card")
+        for num in ownNums:
+            if num in winningNums:
+                if card == 0:
+                    card += 1
+                    print(num, winningNums)
+                    print(card)
+                else:
+                    card *= 2
+                    print(num, winningNums)
+                    print(card)
+        total += card        
+    print(total)
+#part1()
 
-for lines in f:
-    card = 0
-    #split "Card...:" from numbers
-    line = lines.split(':')
-    #split winning numbers from own numbers
-    nums = line[1].strip().split(" | ")
-    winningNums = nums[0].strip().split()
-    ownNums = nums[1].strip().split()
-    #check if own numbers in winning numbers + add to temporal variable "card")
-    for num in ownNums:
-        if num in winningNums:
-            if card == 0:
-                card += 1
-                print(num, winningNums)
-                print(card)
-            else:
-                card *= 2
-                print(num, winningNums)
-                print(card)
-    total += card
-    
-print(total)
-                
+def part2():
+    total = 0
+    for lines in f:
+        card = 0
+        #split "Card...:" from numbers
+        line = lines.split(':')
+        #split winning numbers from own numbers
+        nums = line[1].strip().split(" | ")
+        winningNums = nums[0].strip().split()
+        ownNums = nums[1].strip().split()
+        #check if own numbers in winning numbers + add to temporal variable "card")
+        for num in ownNums:
+            if num in winningNums:
+                if card == 0:
+                    card += 1
+                    print(num, winningNums)
+                    print(card)
+                else:
+                    card *= 2
+                    print(num, winningNums)
+                    print(card)
+        total += card        
+    print(total)
